37cd8b3e0236b2e0a4c01f698f386f270f0a6b9d
Vinícius Scala <viniciusscala@gmail.com>
2024-03-02 16:02:31 -0300

    update actions file to add the search action

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,27 +1,40 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import typesense
 
+class ActionSearchQA(Action):
+    def name(self) -> Text:
+        return "action_search_qa"
 
-# This is a simple example for a custom action which utters "Hello World!"
+    async def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        
+        client = typesense.Client({
+            'nodes': [{
+                'host': 'yq1596od2mxi47vhp-1.a1.typesense.net',
+                'port': '443',
+                'protocol': 'https'
+            }],
+            'api_key': 'k26KOvUUNGPs2LbpWT1cu4sBpoQX7nct',
+            'connection_timeout_seconds': 2
+        })
+        
+        user_message = tracker.latest_message.get("text", "")
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+        search_parameters = {
+        'q'         : user_message,
+        'query_by'  : 'question, answer'
+        }
+
+        collection_name = 'questions'
+        response = client.collections[collection_name].documents.search(search_parameters)
+
+        if response['hits'][0]['document']['answer']:
+            answer = response["hits"][0]["document"]["answer"]
+            dispatcher.utter_message(text=answer)
+        else:
+            dispatcher.utter_message(text="Sorry, I couldn't find an answer to that question.")
+
+        return []
