18a1edf80c09cb3263ddbac0ca2abe6151b366d0
Vinícius Scala <viniciusscala@gmail.com>
2024-03-05 17:03:40 -0300

    add flight search action

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -3,6 +3,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import typesense
+import pandas as pd
 
 class ActionSearchQA(Action):
     def name(self) -> Text:
@@ -37,5 +38,30 @@ class ActionSearchQA(Action):
             dispatcher.utter_message(text=answer)
         else:
             dispatcher.utter_message(text="Sorry, I couldn't find an answer to that question.")
+
+        return []
+    
+class ActionFlightsSearch(Action):
+    def name(self) -> Text:
+        return "action_flights_search"
+
+    async def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        
+        flights_df = pd.read_csv('../data/generated_flights_data.csv')
+        
+        origin = tracker.get_slot('fromloc.city_name')
+        destination = tracker.get_slot('toloc.city_name')
+        day = tracker.get_slot('depart_date.day_name')
+        
+        flights = flights_df[(flights_df['fromloc.city_name'] == origin) & (flights_df['toloc.city_name'] == destination) & (flights_df['depart_date.day_name'] == day)]
+
+        if flights.shape[0] > 0:
+            response = "I found the following flights: \n" + flights.to_string(index=False)
+        else:
+            response = "I couldn't find any flights for that route on that day."
+
+        dispatcher.utter_message(text=response)
 
         return []
